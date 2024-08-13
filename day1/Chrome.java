54deceeb7dacaca588c57db1506d54b63eaf3acd
sivanathcert <92459302+sivanathcert@users.noreply.github.com>
2024-08-13 09:34:16 +0530

    Day1 - Core Java

---
+++ day1/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey ='C';
+		System.out.println("Version: "+version);
+		System.out.println("Developer: "+developer);
+		System.out.println( "Is Beta: "+isBeta
+				+"\n" + "Release Year: "+releaseYear
+				+"\n" + "Shortcut Key: "+shortcutKey);
+
+	}
+
+}
